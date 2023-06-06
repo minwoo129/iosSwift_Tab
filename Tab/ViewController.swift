c022455a9ceab547d95c22bdd395f713e5bfecf6
minwoo129 <key0129mw@naver.com>
2023-06-06 21:52:59 +0900

    메인 탭바 storyboard 화면 레이아웃 배치, 버튼 액션함수 추가

--- Tab/ViewController.swift
+++ Tab/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+    }
 }
 
